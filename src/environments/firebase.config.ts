a5b996edadb8c7f053b606490bbd82759792dabe
Hieu Do <mr.hieudt@gmail.com>
2017-09-03 10:40:58 -0500

    Add script to populate firebase database

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBntsME8w0DEZs2a-ivf2Zi3W9_mxhUNvc',
+  authDomain: 'angular-firebase-demo-86953.firebaseapp.com',
+  databaseURL: 'https://angular-firebase-demo-86953.firebaseio.com',
+  projectId: 'angular-firebase-demo-86953',
+  storageBucket: 'angular-firebase-demo-86953.appspot.com',
+  messagingSenderId: '490595022357'
+};
